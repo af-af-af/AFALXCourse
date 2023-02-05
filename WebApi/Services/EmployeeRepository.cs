248ae42d36ee359f3c72ee2f59fe1e3a85ef506c
Andrzej Frydryszak <cne.studio.akk@gmail.com>
2023-02-05 15:46:13 +0100

    repositories

--- WebApi/Services/EmployeeRepository.cs
+++ WebApi/Services/EmployeeRepository.cs
@@ -1,23 +1,111 @@
-﻿using WebApi.Models;
+﻿using System.Data.SqlClient;
+using WebApi.Models;
 using WebApi.Services.Interfaces;
 
 namespace WebApi.Services
 {
     public class EmployeeRepository : IEmployeeRepository
     {
-        public Task Create(Employee entity)
+        private const string _connectionString = "";
+        
+        public async Task Create(Employee employee)
         {
-            throw new NotImplementedException();
+            var queryString = "insert into Employees(Id, FirstName, LastName, Email, DepartmentId, PaycheckId) " +
+                "values(@id, @firstName, @lastName, @email, @departmentId, @paycheckId)";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@id", employee.Id.ToString());
+                command.Parameters.AddWithValue("@firstName", employee.FirstName);
+                command.Parameters.AddWithValue("@lastName", employee.LastName);
+                command.Parameters.AddWithValue("@email", employee.Email);
+                command.Parameters.AddWithValue("@departmentId", employee.DepartmentId.ToString());
+                command.Parameters.AddWithValue("@paycheckId", employee.PaycheckId.ToString());
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
         }
 
-        public Task<List<Employee>> GetAll()
+        public async Task<List<Employee>> GetAll()
         {
-            throw new NotImplementedException();
+            var employees = new List<Employee>();
+            var queryString = "select * from Employees";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sqlCommand = new SqlCommand(queryString, connection);
+                try
+                {
+                    connection.Open();
+                    var sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        var employee = new Employee
+                        {
+                            Id = Guid.Parse(sqlDataReader[0].ToString()),
+                            FirstName = sqlDataReader[1].ToString(),
+                            LastName = sqlDataReader[2].ToString(),
+                            Email = sqlDataReader[3].ToString(),
+                            DepartmentId = Guid.Parse(sqlDataReader[4].ToString()),
+                            PaycheckId = Guid.Parse(sqlDataReader[5].ToString()),
+                        };
+                        employees.Add(employee);
+                    }
+                    sqlDataReader.Close();
+                    return employees;
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
         }
 
-        public Task<Employee> GetById(Guid Id)
+        public async Task<Employee> GetById(Guid Id)
         {
-            throw new NotImplementedException();
+            var queryString = "select * from Employees where Employees.Id=@id";
+            var employees = new List<Employee>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sqlCommand = new SqlCommand(queryString, connection);
+                sqlCommand.Parameters.AddWithValue("@id", Id.ToString());
+
+                try
+                {
+                    connection.Open();
+                    var sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        var employee = new Employee
+                        {
+                            Id = Guid.Parse(sqlDataReader[0].ToString()),
+                            FirstName = sqlDataReader[1].ToString(),
+                            LastName = sqlDataReader[2].ToString(),
+                            Email = sqlDataReader[3].ToString(),
+                            DepartmentId = Guid.Parse(sqlDataReader[4].ToString()),
+                            PaycheckId = Guid.Parse(sqlDataReader[5].ToString())
+                        };
+                        employees.Add(employee);
+                    }
+                    sqlDataReader.Close();
+                    return employees.FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
